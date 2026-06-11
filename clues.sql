07d7886c8edabda3a5fd1d18f274b1c12e7ca31b
lecharles <charleslozano@gmail.com>
2026-06-10 20:23:53 -0700

    clue 2 vatican official language

--- clues.sql
+++ clues.sql
@@ -6,7 +6,7 @@ SELECT name, population FROM countries WHERE region = 'Southern Europe' ORDER BY
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
-
+SELECT countries.name, countrylanguages.language FROM countries JOIN countrylanguages ON countrylanguages.countrycode = countries.code WHERE countries.name = 'Holy See (Vatican City State)';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
